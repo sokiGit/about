d91aeb46b6265c430e808cebce3ba3a72c746a69
sokiGit <soki.personal@gmail.com>
2024-04-09 12:06:08 +0200

    vite proj

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Button from './components/Button/Button'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <Button>Button testing</Button>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,5 @@
+export default function Button(data) {
+    return (
+        <button>{data.children}</button>
+    );
+}
